4c7ecba845be2e7563d6cf4803e0fbb75a021d11
asamad-dev <samad6846@gmail.com>
2024-12-15 13:58:48 +0500

    Handling invalid command

--- src/main.cs
+++ src/main.cs
@@ -4,7 +4,7 @@ using System.Net.Sockets;
 // Console.WriteLine("Logs from your program will appear here!");
 
 // Uncomment this line to pass the first stage
-Console.Write("$ invalid_command");
+Console.Write("$ ");
 
 // Wait for user input
 var command = Console.ReadLine();
