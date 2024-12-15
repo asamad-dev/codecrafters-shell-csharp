2a7eb16aef6a6764e544b2d91f34218e576f0887
asamad-dev <samad6846@gmail.com>
2024-12-15 14:11:55 +0500

    Handling invalid command

--- src/main.cs
+++ src/main.cs
@@ -3,7 +3,7 @@ using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // Console.WriteLine("Logs from your program will appear here!");
 while (true) {
-
+    Console.Write("$ ");
     var command = Console.ReadLine();
 
     if (command.StartsWith("exit")) {
