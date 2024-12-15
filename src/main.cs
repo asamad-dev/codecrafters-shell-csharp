4a313adbffbcacfda5fc7789d8262b107af52e9a
asamad-dev <samad6846@gmail.com>
2024-12-15 14:07:54 +0500

    Handling invalid command

--- src/main.cs
+++ src/main.cs
@@ -2,12 +2,13 @@ using System.Net;
 using System.Net.Sockets;
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // Console.WriteLine("Logs from your program will appear here!");
-while (true) {
+string userInput = ""
+while (userInput ~= "exit 0") {
 
 // Uncomment this line to pass the first stage
     Console.Write("$ ");
 
 // Wait for user input
-    var userInput = Console.ReadLine();
+    userInput = Console.ReadLine();
     Console.WriteLine("{0}: command not found", userInput);
 }
